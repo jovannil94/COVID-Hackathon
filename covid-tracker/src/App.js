620ea76d7e031500e47d107f81a8c07452ab68fb
jovannil94 <jovanniluna@pursuit.org>
2020-06-17 20:48:32 -0400

    pushing to master

---
+++ covid-tracker/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      COVID TRACKER
+    </div>
+  );
+}
+
+export default App;
