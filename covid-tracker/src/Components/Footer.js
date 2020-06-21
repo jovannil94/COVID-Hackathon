970a3109e5d5fc5a20fa1de417656b76728883e8
DejaLFlynn <dejaflynn@pursuit.org>
2020-06-21 01:30:19 -0400

    starting footer navigation

--- covid-tracker/src/Components/Footer.js
+++ covid-tracker/src/Components/Footer.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React from 'react';
+// import { Grid, Nav, NavItem } from 'react-bootstrap';
 
-const Footer = () => {
+function Footer(/*props*/) {
   return (
-    <>
-      <div>footer</div>
-    </>
+    <div className="footer">Footer</div>
+    // <footer>
+    //   <Grid>
+    //     <Nav justified>
+    //       <NavItem
+    //         eventKey={1}>
+    //         Privacy policy
+    //       </NavItem>
+    //       <NavItem
+    //         eventKey={2}
+    //         title="Item">
+    //         Terms & Conditions
+    //       </NavItem>
+    //       <NavItem
+    //         eventKey={3}>
+    //         Some other professional link
+    //       </NavItem>
+    //     </Nav>
+
+    //     <div className="text-center small copyright">
+    //       © RLM 2016
+    //     </div>
+    //   </Grid>
+    // </footer>
   );
-};
+}
 
 export default Footer;
