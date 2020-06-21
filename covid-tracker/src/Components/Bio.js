e0a3906d692077dd76dd32083ca085bf00237070
DejaLFlynn <dejaflynn@pursuit.org>
2020-06-21 10:44:10 -0400

    footer done

---
+++ covid-tracker/src/Components/Bio.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Bio = () => {
+  return (
+    <>
+      <div>Bio
+          <p>
+              Jovanni Luna
+            <a href="https://www.linkedin.com/in/jovanni-luna-5831bb118/">Linkedin</a>
+                Maria Liza Martinez
+            <a href="https://www.linkedin.com/in/marializamartinez/">Linkedin</a>
+                Deja Flynn
+            <a href="https://www.linkedin.com/in/deja-flynn-991058192/">Linkedin</a>
+          </p>
+      </div>
+    </>
+  );
+};
+
+export default Bio;
