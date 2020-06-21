6f3652658182d2051a327d7f620926db07893886
DejaLFlynn <dejaflynn@pursuit.org>
2020-06-21 12:17:00 -0400

    styling for bio and about

--- covid-tracker/src/Components/Bio.js
+++ covid-tracker/src/Components/Bio.js
@@ -1,14 +1,18 @@
 import React from "react";
+import '../CSS/Bio.css'
 
 const Bio = () => {
   return (
     <>
-      <div>Bio
-          <p>
+      <div className="Bio">Bio
+          <p className="bioparagraph">
+              <br></br>
               Jovanni Luna
             <a target='_blank' href="https://www.linkedin.com/in/jovanni-luna-5831bb118/">Linkedin</a>
+                <br></br>
                 Maria Liza Martinez
             <a target='_blank' href="https://www.linkedin.com/in/marializamartinez/">Linkedin</a>
+                <br></br>
                 Deja Flynn
             <a target='_blank' href="https://www.linkedin.com/in/deja-flynn-991058192/">Linkedin</a>
           </p>
