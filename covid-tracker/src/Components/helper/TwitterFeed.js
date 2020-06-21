2bec6ccf0ac6f6a4b05f66ee1c4f5a473e47bc8f
jovannil94 <jovanniluna@pursuit.org>
2020-06-21 10:33:09 -0400

    adds Twitter Feed

---
+++ covid-tracker/src/Components/helper/TwitterFeed.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+
+const TwitterFeed = ({handle}) => {
+  const [twitterName, setTwitterName] = useState(handle);
+
+  useEffect(() => {
+    if (handle !== undefined) {
+      setTwitterName(handle.slice(1));
+    }
+  }, [handle]);
+
+    return(
+      <div>
+        {twitterName !== undefined ? <TwitterTimelineEmbed sourceType="profile" screenName={twitterName} key={twitterName} options={{height: 100}, {width: 500}}/> : null}
+      </div>
+    )
+}
+  
+  export default TwitterFeed;
