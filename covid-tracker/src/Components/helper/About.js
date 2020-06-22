fa301816a9810cc9c81b54f50599edad62a8de8e
maria liza <mariamartinez@pursuit.org>
2020-06-22 14:18:25 -0400

    finalize bio and about styling

--- covid-tracker/src/Components/helper/About.js
+++ covid-tracker/src/Components/helper/About.js
@@ -1,20 +1,29 @@
 import React from "react";
-import '../../CSS/About.css';
+import "../../CSS/About.css";
 
 const About = () => {
   return (
     <>
-      <div className="paragraph">About Covid Near Us
-          <p>
+      <div className="paragraph">
+        <h1 className="titleTwit">About Covid Near Us</h1>
+        <p>
 
-Covid-19, an illness caused by a virus that can spread from person to person. COVID-19 symptoms can range from mild (or no symptoms) to severe illness. You can become infected by coming into close contact (about 6 feet or two arm lengths) with a person who has COVID-19. It is primarily spread from person to person. COVID-19 cases and deaths have been reported in all 50 states, and the situation is constantly changing. Because travel increases your chances of getting infected and spreading COVID-19, staying home is the best way to protect yourself and others from getting sick.
-Travel increases your chances of getting and spreading COVID-19. We don’t know if one type of travel is safer than others; however, airports, bus stations, train stations, and rest stops are all places travelers can be exposed to the virus in the air and on surfaces. These are also places where it can be hard to social-distance.
-How will we know if the state we desire to travel to may have a surge in positive case numbers, deaths or even available hospital beds.
-The Covid Near Us App is a quick solution to the person who needs to travel and keeps them informed about each state in one app.
-When launched immediately shows Covid-19 stats for the United States. It offers a search by state on the landing page. Once a state has been selected, the user is taken to the search result page that gives hospitalization stats, infected cases, recovered cases, icu numbers and deaths. Also below the search results you can find a testing site “near us” to send the user to a testing site using geolocation.
-Once the area is selected then the user is taken to the Testing site in that location for 3 sites that have testing options for that user. 
-It gives the user the address and phone number to contact testing facilities.
-          </p>
+          The Covid Near Us App is a quick solution to the person who needs to
+          travel and keeps them informed about each state in one app. 
+          <br></br>
+          <br></br>
+          
+          When launched immediately shows Covid-19 stats for the United States. It
+          offers a search by state on the landing page. Once a state has been
+          selected, the user is taken to the search result page that gives
+          hospitalization stats, infected cases, recovered cases, icu numbers
+          and deaths. Also below the search results you can find a testing site
+          “near us” to send the user to a testing site using geolocation. Once
+          the area is selected then the user is taken to the Testing site in
+          that location for 3 sites that have testing options for that user. It
+          gives the user the address and phone number to contact testing
+          facilities.
+        </p>
       </div>
     </>
   );
--- covid-tracker/src/Components/helper/Bio.js
+++ covid-tracker/src/Components/helper/Bio.js
@@ -1,27 +1,57 @@
 import React from "react";
-import '../../CSS/Bio.css'
+import "../../CSS/Bio.css";
+import ourLogo from "../../Images/ourLogo.png";
 
 const Bio = () => {
   return (
     <>
-      <div className="bio">Team
-          <p className="bioparagraph">
-              <br></br>
-              Jovanni Luna
-                Transitioning from one career to become a great Web Developer. 
-                <br></br>
-            <a target='_blank' rel="noopener noreferrer" href="https://www.linkedin.com/in/jovanni-luna-5831bb118/">Linkedin</a>
-                <br></br>
-                Maria Liza Martinez
-                NYU grad with a bachelor of fine arts degree in animation, turned baker, turned chocolatier, looking to land her first job in video game development!
-                <br></br>
-            <a target='_blank' rel="noopener noreferrer" href="https://www.linkedin.com/in/marializamartinez/">Linkedin</a>
-                <br></br>
-                Deja Flynn
-                Former math and science teacher aspiring for a new career in Computer Science/Full-Stack Web Development.
-                <br></br>
-            <a target='_blank' rel="noopener noreferrer" href="https://www.linkedin.com/in/deja-flynn-991058192/">Linkedin</a>
-          </p>
+      <img src={ourLogo} alt="logo" />
+      <div className="bio">
+        <div className="bioparagraph">
+          <div className="bioDivs">
+            <h2 className="titleTwit-name">Jovanni Luna</h2>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/jovanni-luna-5831bb118/"
+            >
+              <p className="linked">Linkedin</p>
+            </a>
+            <p>
+              Transitioning from one career to become a great Web Developer.
+            </p>
+          </div>
+          <div className="bioDivs">
+            <h2 className="titleTwit-name">Maria Martinez</h2>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/marializamartinez/"
+            >
+              <p className="linked">Linkedin</p>
+            </a>
+            <p>
+              NYU grad with a bachelor of fine arts degree in animation, turned
+              baker, turned chocolatier, looking to land her first job in video
+              game development!
+            </p>
+          </div>
+
+          <div className="bioDivs">
+            <h2 className="titleTwit-name">Deja Flynn</h2>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/deja-flynn-991058192/"
+            >
+              <p className="linked">Linkedin</p>
+            </a>
+            <p>
+              Former math and science teacher aspiring for a new career in
+              Computer Science/Full-Stack Web Development.
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
