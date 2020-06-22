992bb1be56bdda5d94ab17cd11a9c44cd4a17488
maria liza <mariamartinez@pursuit.org>
2020-06-22 15:01:25 -0400

    fix

--- covid-tracker/src/Components/helper/StateInfoIndex.js
+++ covid-tracker/src/Components/helper/StateInfoIndex.js
@@ -9,21 +9,28 @@ const StateInfoIndex = ({ info, statePic }) => {
         <li>
           <h1 className="stateName">{info.name}</h1>
         </li>
-        <img className="statePic" src={statePic} alt="state image" width="500"/>
+        <img
+          className="statePic"
+          src={statePic}
+          alt="state image"
+          width="500"
+        />
         <p className="govtsites">Official Government Websites:</p>
 
-        <li
-          className="websites"
-          onClick={() => window.open(`${info.covid19Site}`)}
-        >
-          {info.covid19Site}
-        </li>
-        <li
-          className="websites"
-          onClick={() => window.open(`${info.covid19SiteSecondary}`)}
-        >
-          {info.covid19SiteSecondary}
-        </li>
+        <div className="webContainer">
+          <li
+            className="websites"
+            onClick={() => window.open(`${info.covid19Site}`)}
+          >
+            {info.covid19Site}
+          </li>
+          <li
+            className="websites"
+            onClick={() => window.open(`${info.covid19SiteSecondary}`)}
+          >
+            {info.covid19SiteSecondary}
+          </li>
+        </div>
         <details>
           <summary>Sources</summary>
           <p>{info.notes}</p>
